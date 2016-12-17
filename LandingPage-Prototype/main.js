670e5a6c4610543c9ec530f4cd2d3a9f4c77503c
Absend <at.papucharov@gmail.com>
2016-12-17 19:49:50 +0200

    landing page corrections

--- LandingPage-Prototype/main.js
+++ LandingPage-Prototype/main.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
-    $(".dropdown").hover(
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+'use strict';
+
+function loadTemplate(selector) {
+    let postTemplateHTML = $(selector).html();
+    let compiledTemplate = Handlebars.compile(postTemplateHTML);
+    return $('#content-main').html(compiledTemplate());
+}
+
+$(document).ready(function () {
+    $('.dropdown').hover(
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp('400');
             $(this).toggleClass('open');
         },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown('400');
             $(this).toggleClass('open');
         }
     );
